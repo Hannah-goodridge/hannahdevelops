1ce0553bbaa4f4079371e1f1cf7d3488e691c500
Hannah Goodridge <hannah.goodridge@moneyhub.com>
2025-06-23 18:15:16 +0100

    add scroll bar and selection styles

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -16,7 +16,7 @@ const Square = ({ crop, code, pen, title, delay }: Props) => {
     <div
       className={`bg-gradient-border rounded-xl w-1/3 flex-grow anim-slide-in-bottom m-4 aspect-square ${cssDelayClass}`}
     >
-      <div className="bg-highlight hover:bg-tertiary dark:bg-tertiary dark:hover:bg-highlight hover:text-primary dark:text-white transition duration-300 ease-in-out flex items-center md:items-end p-2 md:p-8 h-full mask-clip-border">
+      <div className="bg-highlight hover:bg-tertiary dark:bg-tertiary dark:hover:bg-highlight hover:text-primary dark:text-white transition duration-300 ease-in-out flex items-center md:items-end p-2 md:p-8 h-full rounded-lg">
         <div className="flex flex-row md:flex-row items-start w-full">
           {crop && <Crop className="w-full md:w-1/3" />}
           {code && <Code className="w-full md:w-1/3" />}
